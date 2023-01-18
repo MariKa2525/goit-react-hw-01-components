266f271c51873fa6f8cab38e6abbf2b2cb9bf731
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-01-18 23:24:28 +0200

    add

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -45,17 +45,3 @@ Profile.propTypes = {
           likes: PropTypes.number.isRequired,
       }).isRequired
 };
-
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired,
-//   stats: PropTypes.objectOf(
-//       PropTypes.shape({
-//           followers: PropTypes.string.isRequired,
-//           views: PropTypes.number.isRequired,
-//           likes: PropTypes.number.isRequired,
-//       })
-//   ).isRequired
-// };
